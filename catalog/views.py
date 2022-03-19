055a805f6333e0c0aeaafb059bbfc50309525605
yeezy_na_izi <howone1sky@gmail.com>
2022-03-19 22:49:21 +0300

    fix display bug, now 'catalog/<pk>' works

--- catalog/views.py
+++ catalog/views.py
@@ -5,5 +5,5 @@ def item_list(request):
     return HttpResponse('<h1>Список элементов</h1>')
 
 
-def item_detail(request):
-    return HttpResponse(f'<h1>Подробно элемент</h1>')
+def item_detail(request, pk):
+    return HttpResponse(f'<h1>Подробно элемент pk={pk}</h1>')
